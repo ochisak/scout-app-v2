7c7a8cae75aca93888b3adb54c9cffc315cc6683
ochisak <chi.hi.hi92@gmail.com>
2025-06-15 18:03:07 +0900

    本番UI追加・修正完了

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useState } from 'react'
-import Section from './components/Section'
+import Section from '../components/Section'
 
 export default function Home() {
   const [job, setJob] = useState('')
